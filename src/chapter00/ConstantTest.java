3efc0217ecb3eefaded9225c602481bf09ded075
user <user@user-PC>
2016-09-05 21:19:52 +0900

    변수 종류 추가

--- src/chapter00/ConstantTest.java
+++ src/chapter00/ConstantTest.java
@@ -4,10 +4,19 @@ public class ConstantTest {
 
 	public static void main(String[] args) {
 		
+	
 		final int CONST_NUMBER = 10; 
 		//상수(Constant) 변경될 수 없는 고정된 데이터(변경하면 에러남)
 		//상수는 두번째 대입이 안됨
 		//상수는 대문자로 써주는게 관례
+		
+		//public, protected, private - 접근 제어자
+		//final 	- 값 수정 제한 제어자
+		//abstract 	- 구현 필요 제어자
+		//static	- 하나의 인스턴스만 허용하는 제어자
+		//synchronized - 동시 접근 제어자
+		
+		
 		System.out.println(CONST_NUMBER);
 		
 		int number = 10;			//변수(Variable)
--- src/chapter00/VariableTest.java
+++ src/chapter00/VariableTest.java
@@ -37,6 +37,9 @@ public class VariableTest {
 		String str2 = "Hello World";
 		System.out.println(str2);
 		
+//		기본 자료형은 값을 바로 지정하면 되지만, 참조자료형은 일반적으로 new와 생성자를 지정하여 객체 생성
+		
+		
 		//charcter 형 
 		char ch1 = '1';
 		char ch2 = 49;
@@ -45,5 +48,14 @@ public class VariableTest {
 		System.out.println(ch1 + ":" + ch2 + ":" + ch3);
 		//커밋확인
 		//다운로드후 커밋확인
+		
+/*		public class VariableTypes{
+			int instanceVariable;			   	//인스턴스변수 - "객체가 생성될 때" 생명시작 그 객체를 참조하고 있는 다른객체없으면 소멸
+			static int classVariable;		  	//클래스 변수 - "클래스 생성시" 생명시작 자바프로그램 끝날 때 소멸
+		public void method(int parameter){ 		//매개 변수 - "메소드 호출시" 생명 시작 메소드 끝나면 소멸
+			int localVariable; 			   		//지역 변수 - "지역 변수를 선언한 곳(중괄호)에서부터" 생명 시작 지역변수를 선언한 중괄호 끝나면 소멸
+			}
+		}
+*/
 	}
 }
