d6b7128e82343b5794d1c98256d85e7d7cf80b21
rlawlsdnjs <orochi13@naver.com>
2016-09-07 12:31:57 +0900

    d

--- src/chapter00/Guguadan02.java
+++ src/chapter00/Guguadan02.java
@@ -3,7 +3,7 @@ package chapter00;
 public class Guguadan02 {
 	public static void main(String[] args) {
 		
-		int result;
+/*		int result;
 		
 		for(int i = 1; i<10; i++){
 			for(int j = 1; j<10; j++){			
@@ -15,11 +15,23 @@ public class Guguadan02 {
 		}
 
 	}
+		*/
+		for(int i = 1; i<10; i++){
+			for(int j = 1; j<10; j++){	
+				
+				int result = i*j;
+				
+				System.out.print(j + "X" + i + "=" + result + "\t");
+				
+				}
+			System.out.println();
+			}
+				
+		}
+
+	}
 		
 		
 	//	print(i+"*"+j+"="+(i*j));
-	
-	}
-}
 
 //1단 2단 3단 4단 이런식으로 과제
